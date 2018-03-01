f7949ada656d945b76dd8df59275c41e2a606d3f
Jake Huxell <darkduckmovies@gmail.com>
2018-03-01 18:08:53 +0000

    EmoteLocalUIElementComponent

---
+++ Assets/Editor/UnitTests/UI/Local/EmoteLocalUIElementComponentTests.cs
@@ -0,0 +1,93 @@
+﻿// Copyright (C) Threetee Gang All Rights Reserved
+
+using System.Collections.Generic;
+using Assets.Scripts.Components.Emote;
+using Assets.Scripts.Messaging;
+using Assets.Scripts.UI.Local;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Editor.UnitTests.UI.Local
+{
+    [TestFixture]
+    public class EmoteLocalUIElementComponentTestFixture 
+    {
+        private EmoteLocalUIElementComponent _emote;
+
+        private Image _image;
+        private Image _otherImage;
+
+        private UnityMessageEventDispatcher _dispatcher;
+	
+        [SetUp]
+        public void BeforeTest()
+        {
+            _dispatcher = new UnityMessageEventDispatcher();
+
+            _emote = new GameObject().AddComponent<EmoteLocalUIElementComponent>();
+
+            _image = new GameObject().AddComponent<Image>();
+            _otherImage = new GameObject().AddComponent<Image>();
+
+            _emote.EmoteGraphics = new List<EmoteGraphicEntry>
+            {
+                new EmoteGraphicEntry{DisplayImage = _image, State = EEmoteState.Alerted},
+                new EmoteGraphicEntry{DisplayImage = _otherImage, State = EEmoteState.None}
+            };
+
+            _emote.OnElementInitialised(_dispatcher);
+        }
+	
+        [TearDown]
+        public void AfterTest()
+        {
+            _emote.OnElementUninitialised();
+
+            _otherImage = null;
+            _image = null;
+
+            _emote = null;
+
+            _dispatcher = null;
+        }
+	
+        [Test]
+        public void Initialised_SetsAllImagesToInactive() 
+        {
+            Assert.IsFalse(_image.gameObject.activeSelf);
+            Assert.IsFalse(_image.gameObject.activeSelf);
+        }
+
+        [Test]
+        public void ReceivesStateChangeMessage_RelevantMessageSetActive()
+        {
+            const EEmoteState state = EEmoteState.Alerted;
+
+            _dispatcher.InvokeMessageEvent(new EmoteStatusChangedUIMessage(state));
+
+            Assert.IsTrue(_emote.EmoteGraphics.Find((image) => image.State == state).DisplayImage.gameObject.activeSelf);
+        }
+
+        [Test]
+        public void ReceivesStateChangeMessage_OldImageSetInactive()
+        {
+            const EEmoteState state = EEmoteState.Alerted;
+
+            _dispatcher.InvokeMessageEvent(new EmoteStatusChangedUIMessage(state));
+            _dispatcher.InvokeMessageEvent(new EmoteStatusChangedUIMessage(EEmoteState.None));
+
+            Assert.IsFalse(_emote.EmoteGraphics.Find((image) => image.State == state).DisplayImage.gameObject.activeSelf);
+        }
+
+        [Test]
+        public void ReceivesStateChangeMessage_NoneStateRemainsInactive()
+        {
+            const EEmoteState state = EEmoteState.None;
+
+            _dispatcher.InvokeMessageEvent(new EmoteStatusChangedUIMessage(state));
+
+            Assert.IsFalse(_emote.EmoteGraphics.Find((image) => image.State == state).DisplayImage.gameObject.activeSelf);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Local/EmoteLocalUIElementComponent.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (C) Threetee Gang All Rights Reserved
+
+using System.Collections.Generic;
+using Assets.Scripts.Components.Emote;
+using Assets.Scripts.Messaging;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI.Local
+{
+    [System.Serializable]
+    public class EmoteGraphicEntry
+    {
+        public EEmoteState State;
+        public Image DisplayImage;
+    }
+
+    public class EmoteLocalUIElementComponent 
+        : LocalUIElementComponent
+    {
+        public List<EmoteGraphicEntry> EmoteGraphics;
+
+        private Dictionary<EEmoteState, Image> _emoteMappings;
+        private UnityMessageEventHandle<EmoteStatusChangedUIMessage> _emoteStateChangedHandle;
+        private EEmoteState _currentState;
+
+        protected override void OnElementInitialisedImpl()
+        {
+            _currentState = EEmoteState.None;
+
+            SetMappings();
+            RegisterMessages();
+        }
+
+        protected override void OnElementUninitialisedImpl()
+        {
+            UnregisterMessages();
+            ClearMappings();
+        }
+
+        private void SetMappings()
+        {
+            _emoteMappings = new Dictionary<EEmoteState, Image>();
+
+            foreach (var emoteGraphic in EmoteGraphics)
+            {
+                _emoteMappings.Add(emoteGraphic.State, emoteGraphic.DisplayImage);
+                emoteGraphic.DisplayImage.gameObject.SetActive(false);
+            }
+        }
+
+        private void ClearMappings()
+        {
+            _emoteMappings.Clear();
+        }
+
+        private void RegisterMessages()
+        {
+            _emoteStateChangedHandle = Dispatcher.RegisterForMessageEvent<EmoteStatusChangedUIMessage>(OnEmoteStateChanged);
+        }
+
+        private void UnregisterMessages()
+        {
+            Dispatcher.UnregisterForMessageEvent(_emoteStateChangedHandle);
+        }
+
+        private void OnEmoteStateChanged(EmoteStatusChangedUIMessage inMessage)
+        {
+            _emoteMappings[_currentState].gameObject.SetActive(false);
+
+            _currentState = inMessage.State;
+
+            if (_currentState != EEmoteState.None)
+            {
+                _emoteMappings[_currentState].gameObject.SetActive(true);
+            }
+        }
+    }
+}
