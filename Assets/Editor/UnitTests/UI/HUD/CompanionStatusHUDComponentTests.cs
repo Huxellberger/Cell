920b3e6a0e22ac71456f71ed0c759945e48f919b
Jake Huxell <darkduckmovies@gmail.com>
2018-03-24 09:58:45 +0000

    CompanionStatusHUD element added

---
+++ Assets/Editor/UnitTests/UI/HUD/CompanionStatusHUDComponentTests.cs
@@ -0,0 +1,157 @@
+﻿// Copyright (C) Threetee Gang All Rights Reserved
+
+using System.Collections.Generic;
+using Assets.Scripts.AI.Companion;
+using Assets.Scripts.Messaging;
+using Assets.Scripts.Test.UI.HUD;
+using Assets.Scripts.UI.HUD;
+using Castle.Core.Internal;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Editor.UnitTests.UI.HUD
+{
+    [TestFixture]
+    public class CompanionStatusHUDComponentTestFixture
+    {
+        public const string SpritePath = "Test/Sprites/TestSprite";
+
+        private TestCompanionStatusHUDComponent _status;
+        private CompanionStatusHUDComponent.CompanionStatus _primaryStatusElement;
+        private CompanionStatusHUDComponent.CompanionStatus _secondaryStatusElement;
+
+        private Sprite _setSprite;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            _status = new GameObject().AddComponent<TestCompanionStatusHUDComponent>();
+            _status.TestDispatcher = new UnityMessageEventDispatcher();
+            _status.IconDisableAlpha = 0.3f;
+
+            _primaryStatusElement = new CompanionStatusHUDComponent.CompanionStatus
+            {
+                CompanionSlider = new GameObject().AddComponent<Slider>(),
+                CompanionSliderFill = new GameObject().AddComponent<Image>(),
+                CompanionUseCountText = new GameObject().AddComponent<Text>()
+            };
+
+            _secondaryStatusElement = new CompanionStatusHUDComponent.CompanionStatus
+            {
+                CompanionSlider = new GameObject().AddComponent<Slider>(),
+                CompanionSliderFill = new GameObject().AddComponent<Image>(),
+                CompanionUseCountText = new GameObject().AddComponent<Text>()
+            };
+
+            _status.CompanionStates.Add(new CompanionStatusHUDComponent.CompanionStatusPairing{Slot = ECompanionSlot.Primary, Status = _primaryStatusElement});
+            _status.CompanionStates.Add(new CompanionStatusHUDComponent.CompanionStatusPairing { Slot = ECompanionSlot.Secondary, Status = _secondaryStatusElement });
+
+            _setSprite = Resources.Load<Sprite>(SpritePath);
+
+            _status.TestStart();
+        }
+	
+        [TearDown]
+        public void AfterTest()
+        {
+            _status.TestDestroy();
+
+            _setSprite = null;
+
+            _secondaryStatusElement = null;
+            _primaryStatusElement = null;
+            _status = null;
+        }
+	
+        [Test]
+        public void ReceiveSlotsUpdatedUIMessage_UpdatesSlotsWithNewData() 
+        {
+            var primaryMessageData = new PriorCompanionSlotState{PriorCompanion = null, PriorActive = false, PriorUseCount = 2, PriorUIIcon = _setSprite, PriorCooldown = 1.0f};
+            var secondaryMessageData = new PriorCompanionSlotState { PriorCompanion = null, PriorActive = true, PriorUseCount = 1, PriorUIIcon = _setSprite, PriorCooldown = 0.0f }; 
+
+            _status.TestDispatcher.InvokeMessageEvent(new CompanionSlotsUpdatedUIMessage(new Dictionary<ECompanionSlot, PriorCompanionSlotState>
+                {
+                {ECompanionSlot.Primary, primaryMessageData },
+                {ECompanionSlot.Secondary, secondaryMessageData }
+                }
+            ));
+
+            AssertSlotUpdated(_primaryStatusElement, primaryMessageData);
+            AssertSlotUpdated(_secondaryStatusElement, secondaryMessageData);
+        }
+
+        [Test]
+        public void ReceiveSlotsUpdatedUIMessage_CountNonZero_SetTextColorToWhite()
+        {
+            var primaryMessageData = new PriorCompanionSlotState { PriorCompanion = null, PriorActive = false, PriorUseCount = 1, PriorUIIcon = _setSprite, PriorCooldown = 1.0f };
+            var secondaryMessageData = new PriorCompanionSlotState { PriorCompanion = null, PriorActive = true, PriorUseCount = 1, PriorUIIcon = _setSprite, PriorCooldown = 0.0f };
+
+            _status.TestDispatcher.InvokeMessageEvent(new CompanionSlotsUpdatedUIMessage(new Dictionary<ECompanionSlot, PriorCompanionSlotState>
+                {
+                    {ECompanionSlot.Primary, primaryMessageData },
+                    {ECompanionSlot.Secondary, secondaryMessageData }
+                }
+            ));
+
+            Assert.AreEqual(Color.white, _primaryStatusElement.CompanionUseCountText.color);
+        }
+
+        [Test]
+        public void ReceiveSlotsUpdatedUIMessage_CountZero_SetTextColorToRed()
+        {
+            var primaryMessageData = new PriorCompanionSlotState { PriorCompanion = null, PriorActive = false, PriorUseCount = 0, PriorUIIcon = _setSprite, PriorCooldown = 1.0f };
+            var secondaryMessageData = new PriorCompanionSlotState { PriorCompanion = null, PriorActive = true, PriorUseCount = 1, PriorUIIcon = _setSprite, PriorCooldown = 0.0f };
+
+            _status.TestDispatcher.InvokeMessageEvent(new CompanionSlotsUpdatedUIMessage(new Dictionary<ECompanionSlot, PriorCompanionSlotState>
+                {
+                    {ECompanionSlot.Primary, primaryMessageData },
+                    {ECompanionSlot.Secondary, secondaryMessageData }
+                }
+            ));
+
+            Assert.AreEqual(Color.red, _primaryStatusElement.CompanionUseCountText.color);
+        }
+
+        [Test]
+        public void ReceiveSlotsUpdatedUIMessage_CountUnlimited_SetTextColorToEmpty()
+        {
+            var primaryMessageData = new PriorCompanionSlotState { PriorCompanion = null, PriorActive = false, PriorUseCount = CompanionConstants.UnlimitedCharges, PriorUIIcon = _setSprite, PriorCooldown = 1.0f };
+            var secondaryMessageData = new PriorCompanionSlotState { PriorCompanion = null, PriorActive = true, PriorUseCount = 1, PriorUIIcon = _setSprite, PriorCooldown = 0.0f };
+
+            _status.TestDispatcher.InvokeMessageEvent(new CompanionSlotsUpdatedUIMessage(new Dictionary<ECompanionSlot, PriorCompanionSlotState>
+                {
+                    {ECompanionSlot.Primary, primaryMessageData },
+                    {ECompanionSlot.Secondary, secondaryMessageData }
+                }
+            ));
+
+            Assert.IsTrue(_primaryStatusElement.CompanionUseCountText.text.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void ReceiveSlotsUpdatedUIMessage_ImagePropertiesAreAsExpected()
+        {
+            var primaryMessageData = new PriorCompanionSlotState { PriorCompanion = null, PriorActive = false, PriorUseCount = CompanionConstants.UnlimitedCharges, PriorUIIcon = _setSprite, PriorCooldown = 1.0f };
+            var secondaryMessageData = new PriorCompanionSlotState { PriorCompanion = null, PriorActive = true, PriorUseCount = 1, PriorUIIcon = _setSprite, PriorCooldown = 0.0f };
+
+            _status.TestDispatcher.InvokeMessageEvent(new CompanionSlotsUpdatedUIMessage(new Dictionary<ECompanionSlot, PriorCompanionSlotState>
+                {
+                    {ECompanionSlot.Primary, primaryMessageData },
+                    {ECompanionSlot.Secondary, secondaryMessageData }
+                }
+            ));
+
+            Assert.AreEqual(Image.FillMethod.Vertical, _primaryStatusElement.CompanionSliderFill.fillMethod);
+            Assert.AreEqual(Image.Type.Filled, _primaryStatusElement.CompanionSliderFill.type);
+        }
+
+        private void AssertSlotUpdated(CompanionStatusHUDComponent.CompanionStatus status,
+            PriorCompanionSlotState state)
+        {
+            Assert.AreEqual(status.CompanionUseCountText.text, state.PriorUseCount.ToString());
+            Assert.AreSame(status.CompanionSliderFill.sprite, state.PriorUIIcon);
+            Assert.AreEqual(status.CompanionSlider.value, state.PriorCooldown);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/HUD/CompanionStatusHUDComponent.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (C) Threetee Gang All Rights Reserved
+
+using System;
+using System.Collections.Generic;
+using Assets.Scripts.AI.Companion;
+using Assets.Scripts.Messaging;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI.HUD
+{
+    public class CompanionStatusHUDComponent 
+        : UIComponent
+    {
+        [Serializable]
+        public class CompanionStatus
+        {
+            public Slider CompanionSlider;
+            public Image CompanionSliderFill;
+            public Text CompanionUseCountText;
+        }
+
+        [Serializable]
+        public class CompanionStatusPairing
+        {
+            public ECompanionSlot Slot;
+            public CompanionStatus Status;
+        }
+
+        public List<CompanionStatusPairing> CompanionStates = new List<CompanionStatusPairing>();
+        public float IconDisableAlpha = 0.2f;
+
+        private readonly Dictionary<ECompanionSlot, CompanionStatus> _companionSliders = new Dictionary<ECompanionSlot, CompanionStatus>();
+        private UnityMessageEventHandle<CompanionSlotsUpdatedUIMessage> _slotsUpdatedHandle;
+
+        protected override void OnStart()
+        {
+            foreach (var companionStatus in CompanionStates)
+            {
+                _companionSliders.Add(companionStatus.Slot, companionStatus.Status);
+            }
+
+            _slotsUpdatedHandle = Dispatcher.RegisterForMessageEvent<CompanionSlotsUpdatedUIMessage>(OnSlotsUpdated);
+        }
+
+        protected override void OnEnd()
+        {
+            Dispatcher.UnregisterForMessageEvent(_slotsUpdatedHandle);
+        }
+
+        private void OnSlotsUpdated(CompanionSlotsUpdatedUIMessage inMessage)
+        {
+            foreach (var slotPairing in inMessage.Updates)
+            {
+                if (_companionSliders.ContainsKey(slotPairing.Key))
+                {
+                    UpdateCompanionStatus(_companionSliders[slotPairing.Key], slotPairing.Value);
+                }
+            }
+        }
+
+        private void UpdateCompanionStatus(CompanionStatus inStatus, PriorCompanionSlotState inState)
+        {
+            // Image
+            inStatus.CompanionSliderFill.sprite = inState.PriorUIIcon;
+
+            // Slider value
+            inStatus.CompanionSlider.value = inState.PriorCooldown;
+
+            // Use Count
+            if (inState.PriorUseCount == CompanionConstants.UnlimitedCharges)
+            {
+                inStatus.CompanionUseCountText.text = "";
+            }
+            else
+            {
+                inStatus.CompanionUseCountText.text = inState.PriorUseCount.ToString();
+
+                inStatus.CompanionUseCountText.color = inState.PriorUseCount == 0 ? Color.red : Color.white;
+            }
+
+            // Active
+            inStatus.CompanionSliderFill.CrossFadeAlpha(inState.PriorActive ? 1.0f : IconDisableAlpha, 0.0f, true);
+            inStatus.CompanionSliderFill.type = Image.Type.Filled;
+            inStatus.CompanionSliderFill.fillMethod = Image.FillMethod.Vertical;
+        }
+    }
+}
